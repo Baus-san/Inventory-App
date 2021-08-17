a69df77314e3190988ef809174e50bc57960e545
Baus-san <edzonbausa33@gmail.com>
2021-08-17 12:30:57 +0800

    Add a ViewModel

---
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.inventory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+
+/**
+ * To save the app's transient data and to also access the database, you need a ViewModel.
+ * [ViewModel] will interact with the database via the DAO, and provide data to the UI.
+ * All database operations will have to be run away from the main UI thread,
+ * you'll do that using coroutines and viewModelScope.
+ */
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+    /**
+     * Launching a new coroutine to insert an item in a non-blocking way
+     */
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    /**
+     * Returns an instance of the [Item] entity class with the item info entered by the user.
+     * This will be used to add a new entry to the Inventory database.
+     */
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    /**
+     * Inserts the new Item into database.
+     * Uses the [getNewItemEntry] and [insertItem] function
+     */
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+}
+
+/**
+ * Factory class to instantiate the [ViewModel] instance.
+ */
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
