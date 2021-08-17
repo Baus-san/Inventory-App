81b863783b5fc96ab37884275f6e8a55085aea00
Baus-san <edzonbausa33@gmail.com>
2021-08-17 09:37:08 +0800

    Create an Item Entity

---
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -0,0 +1,24 @@
+package com.example.inventory.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/*
+* An Entity class that Define fields to store the following inventory information for each item.
+* An Int to store the primary key.
+* A String to store the item name.
+* A double to store the item price.
+* An Int to store the quantity in stock.
+*/
+@Entity(tableName = "item")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "name")
+    val itemName: String,
+    @ColumnInfo(name = "price")
+    val itemPrice: Double,
+    @ColumnInfo(name = "quantity")
+    val quantityInStock: Int
+)
